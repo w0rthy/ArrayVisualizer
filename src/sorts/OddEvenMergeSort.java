0598d8201d08a1ceb309e9f7a7a12d4bc7faed88
Piotr Grochowski <piotrunio-2004@wp.pl>
2019-10-19 14:32:48 +0200

    odd-even merge sort bugfixes, recursive comb sort
    odd-even merge sort bugfixes, recursive comb sort

--- src/sorts/OddEvenMergeSort.java
+++ src/sorts/OddEvenMergeSort.java
@@ -27,45 +27,46 @@ final public class OddEvenMergeSort extends Sort {
         this.isBogoSort(false);
     }
     
-    private void oddEvenMergeCompare(int[] array, int i, int j) {
+    private void oddEvenMergeCompare(int[] array, int i, int j, double sleep) {
+        Delays.sleep(sleep);
         if (Reads.compare(array[i], array[j]) > 0)
-            Writes.swap(array, i, j, 1, true, false);
+            Writes.swap(array, i, j, sleep, true, false);
     }
     
     /** lo is the starting position and
      *  n is the length of the piece to be merged,
      *  r is the distance of the elements to be compared
      */
-    private void oddEvenMerge(int[] array, int lo, int n, int r) {
+    private void oddEvenMerge(int[] array, int lo, int n, int r, double sleep) {
         int m = r * 2;
         if (m < n) {
-            this.oddEvenMerge(array, lo, n, m);      // even subsequence
-            this.oddEvenMerge(array, lo+r, n, m);    // odd subsequence
+            this.oddEvenMerge(array, lo, n, m, sleep);      // even subsequence
+            this.oddEvenMerge(array, lo+r, n, m, sleep);    // odd subsequence
             
             for (int i = lo + r; i + r < lo + n; i += m) {
                 Highlights.markArray(1, i);
                 Highlights.markArray(2, i + r);
-                this.oddEvenMergeCompare(array, i, i + r);
+                this.oddEvenMergeCompare(array, i, i + r, sleep);
             }
         }
         else {
             Highlights.markArray(1, lo + r);
             Highlights.markArray(2, lo);
-            this.oddEvenMergeCompare(array, lo, lo+r);
+            this.oddEvenMergeCompare(array, lo, lo+r, sleep);
         }
     }
     
-    private void oddEvenMergeSort(int[] array, int lo, int n) {
+    private void oddEvenMergeSort(int[] array, int lo, int n, double sleep) {
 		if (n > 1) {
 			int m = n / 2;
-			this.oddEvenMergeSort(array, lo, m);
-			this.oddEvenMergeSort(array, lo + m, m);
-			this.oddEvenMerge(array, lo, n, 1);
+			this.oddEvenMergeSort(array, lo, m, sleep);
+			this.oddEvenMergeSort(array, lo + m, m, sleep);
+			this.oddEvenMerge(array, lo, n, 1, sleep);
 		}
 	}
 
     @Override
     public void runSort(int[] array, int currentLength, int bucketCount) {
-        this.oddEvenMergeSort(array, 0, currentLength);
+        this.oddEvenMergeSort(array, 0, currentLength, 1);
     }
 }
---
+++ src/sorts/RecuCombSort.java
@@ -0,0 +1,105 @@
+package sorts;
+
+import templates.Sort;
+import utils.Delays;
+import utils.Highlights;
+import utils.Reads;
+import utils.Writes;
+
+/*
+ * 
+MIT License
+
+Copyright (c) 2019 w0rthy
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ *
+ */
+
+final public class RecuCombSort extends Sort {
+    public RecuCombSort(Delays delayOps, Highlights markOps, Reads readOps, Writes writeOps) {
+        super(delayOps, markOps, readOps, writeOps);
+        
+        this.setSortPromptID("Recursive Comb");
+        this.setRunAllID("Recursive Comb Sort");
+        this.setReportSortID("Recursive Comb Sort");
+        this.setCategory("Exchange Sorts");
+        this.isComparisonBased(true);
+        this.isBucketSort(false);
+        this.isRadixSort(false);
+        this.isUnreasonablySlow(false);
+        this.setUnreasonableLimit(0);
+        this.isBogoSort(false);
+    }
+
+    private void recursivecomb(int[] array, int start, int end, int gap, double sleep) {
+        if (start == (end - gap)){
+            return;
+        }
+        if (((end - start) / gap) % 2 == 0){
+            this.recursivecomb(array, start, end, gap * 2, sleep);
+            this.recursivecomb(array, start + gap, end + gap, gap * 2, sleep);
+        }
+        else{
+            this.recursivecomb(array, start, end + gap, gap * 2, sleep);
+            this.recursivecomb(array, start + gap, end, gap * 2, sleep);
+        }
+        this.finishrecucomb(array, start, end, gap, sleep);
+    }
+
+    private void finishrecucomb(int[] array, int start, int end, int gap, double sleep) {
+        if (start >= (end - gap)){
+            return;
+        }
+        if (((end - start) / gap) % 3 == 2){
+            this.finishrecucomb(array, start, end + gap, gap * 3, sleep);
+            this.finishrecucomb(array, start + gap, end + gap + gap, gap * 3, sleep);
+            this.finishrecucomb(array, start + gap + gap, end, gap * 3, sleep);
+        }
+        else{
+        if (((end - start) / gap) % 3 == 1){
+            this.finishrecucomb(array, start, end + gap + gap, gap * 3, sleep);
+            this.finishrecucomb(array, start + gap, end, gap * 3, sleep);
+            this.finishrecucomb(array, start + gap + gap, end + gap, gap * 3, sleep);
+        }
+        else{
+            this.finishrecucomb(array, start, end, gap * 3, sleep);
+            this.finishrecucomb(array, start + gap, end + gap, gap * 3, sleep);
+            this.finishrecucomb(array, start + gap + gap, end + gap + gap, gap * 3, sleep);
+        }
+        }
+        for (int i = start; i < (end - gap); i += gap){
+            Delays.sleep(sleep);
+            Highlights.markArray(1, i);
+            Highlights.markArray(2, i + gap);
+            if(Reads.compare(array[i], array[i + gap]) == 1) {
+                Writes.swap(array, i, i + gap, sleep, true, false);
+            }
+        }
+    }
+    
+    public void customSort(int[] array, int start, int end) {
+        this.recursivecomb(array, start, end, 1, 1);
+    }
+    
+    @Override
+    public void runSort(int[] array, int length, int bucketCount) {
+        this.recursivecomb(array, 0, length, 1, 1);
+    }
+}
