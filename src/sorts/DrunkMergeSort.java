24c973d78064d57e29a344094a7f6fef14de39db
Piotr Grochowski <piotrunio-2004@wp.pl>
2019-10-20 10:54:48 +0200

    update again
    update again

---
+++ src/sorts/DrunkMergeSort.java
@@ -0,0 +1,104 @@
+package sorts;
+
+import templates.Sort;
+import utils.Delays;
+import utils.Highlights;
+import utils.Reads;
+import utils.Writes;
+
+/*
+ * 
+MIT License
+
+Copyright (c) 2019 w0rthy
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ *
+ */
+
+final public class DrunkMergeSort extends Sort {
+    public DrunkMergeSort(Delays delayOps, Highlights markOps, Reads readOps, Writes writeOps) {
+        super(delayOps, markOps, readOps, writeOps);
+        
+        this.setSortPromptID("Drunk Merge");
+        this.setRunAllID("Drunk Merge Sort");
+        this.setReportSortID("Drunk Mergesort");
+        this.setCategory("Hybrid Sorts");
+        this.isComparisonBased(true);
+        this.isBucketSort(false);
+        this.isRadixSort(false);
+        this.isUnreasonablySlow(false);
+        this.setUnreasonableLimit(0);
+        this.isBogoSort(false);
+    }
+
+    private void drunkInsert(int[] arr, int start, int end) {
+        int pos;
+        
+        for(int j = start; j < end; j++){
+            pos = j;
+            
+            Highlights.markArray(1, j);
+            Highlights.clearMark(2);
+            
+            while(pos > start && Reads.compare(arr[pos], arr[pos - 1]) < 1) {
+                Writes.swap(arr, pos, pos - 1, 0.2, true, false);
+                pos--;
+            }
+        }
+    }
+	
+	private void drunkMerge(int[] arr, int min, int max, int mid) {
+	    int i = 1;
+	    int target = (mid - min);
+	    
+	    while(i <= target) {
+	        Writes.multiSwap(arr, mid + i, min + (i * 2) - 1, 0.05, true, false);
+	        i++;
+	    }
+	    
+	    this.drunkInsert(arr, min, max + 1);
+	}
+
+	private void drunkMergeSort(int[] array, int min, int max) {
+		if(max - min == 0) {      //only one element.
+			Delays.sleep(1);      //no swap
+		}
+		else if(max - min == 1) { //only two elements and swaps them
+			if(Reads.compare(array[min], array[max]) == 1) {
+				Writes.swap(array, min, max, 0.01, true, false);
+			}
+		}
+		else {
+			int mid = (int) Math.floor((min + max) / 2); //The midpoint
+
+			this.drunkMergeSort(array, min, mid);     //sort the left side
+			this.drunkMergeSort(array, mid + 1, max); //sort the right side
+			this.drunkMerge(array, min, max, mid);    //combines them
+			this.drunkMergeSort(array, min, mid);     //sort the left side
+			this.drunkMergeSort(array, mid + 1, max); //sort the right side
+			this.drunkMerge(array, min, max, mid);    //combines them
+		}
+	}
+
+    @Override
+    public void runSort(int[] array, int currentLength, int bucketCount) {
+        this.drunkMergeSort(array, 0, currentLength - 1);
+    }
+}
