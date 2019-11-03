bad5a9e23c030f04b0933e8584f1a33b6f52dfe5
Piotr Grochowski <piotrunio-2004@wp.pl>
2019-11-03 07:52:31 +0100

    runs testing
    runs testing

---
+++ src/sorts/TimSortRuns.java
@@ -0,0 +1,54 @@
+package sorts;
+
+import templates.Sort;
+import templates.TimSortingRuns;
+import utils.Delays;
+import utils.Highlights;
+import utils.Reads;
+import utils.Writes;
+
+/*
+ * Copyright (C) 2008 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class TimSortRuns extends Sort {
+    private TimSortingRuns timSortInstance; // TimSort cannot be simply written off as an abstract class, as it creates an instance of itself
+                                        // in order to track its state. Plus, it contains both instance and static methods, requiring even
+                                        // more refactoring, which would be just doing unnecessary busy work. Instead of what we've done for
+                                        // the rest of the algorithms, we'll favor composition over inheritance here and pass "util" objects
+                                        // to it.
+    
+    public TimSortRuns(Delays delayOps, Highlights markOps, Reads readOps, Writes writeOps) {
+        super(delayOps, markOps, readOps, writeOps);
+        
+        this.setSortPromptID("Tim (runs)");
+        this.setRunAllID("Tim Sort Runs");
+        this.setReportSortID("Tim Sort Runs");
+        this.setCategory("Hybrid Sorts");
+        this.isComparisonBased(true);
+        this.isBucketSort(false);
+        this.isRadixSort(false);
+        this.isUnreasonablySlow(false);
+        this.setUnreasonableLimit(0);
+        this.isBogoSort(false);
+    }
+    
+    @Override
+    public void runSort(int[] array, int currentLength, int bucketCount) {
+        this.timSortInstance = new TimSortingRuns(array, currentLength, this.Highlights, this.Reads, this.Writes);
+        
+        TimSortingRuns.sort(this.timSortInstance, array, currentLength);
+    }
+}
