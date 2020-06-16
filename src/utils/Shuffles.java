0c940fb73b5861ebeeecd1d4d55f632a6e227225
fundamentals-fontdevelopment <66462169+fundamentals-fontdevelopment@users.noreply.github.com>
2020-06-16 17:54:44 +0200

    Update over
    Update over

--- src/utils/Shuffles.java
+++ src/utils/Shuffles.java
@@ -34,8 +34,24 @@ public enum Shuffles {
         public void shuffleArray(int[] array, ArrayVisualizer ArrayVisualizer, Delays Delays, Highlights Highlights, Writes Writes) {
             int currentLen = ArrayVisualizer.getCurrentLength();
             
+            for(int i = currentLen; i > 1; i--){
+                int temp = (int)(Math.random()*i);
+                Reads.play2(array[i-1], array[temp]);
+                Writes.swap(array, i-1, temp, 0, true, false);
+                
+                if(ArrayVisualizer.shuffleEnabled()) Delays.sleep(1);
+            }
+        }
+    },
+    RANDOM1 {
+        @Override
+        public void shuffleArray(int[] array, ArrayVisualizer ArrayVisualizer, Delays Delays, Highlights Highlights, Writes Writes) {
+            int currentLen = ArrayVisualizer.getCurrentLength();
+            
             for(int i = 0; i < currentLen; i++){
-                Writes.swap(array, i, (int)(Math.random()*(currentLen - i)) + i, 0, true, false);
+                int temp = (int)(Math.random()*(currentLen - i)) + i;
+                Reads.play2(array[i], array[temp]);
+                Writes.swap(array, i, temp, 0, true, false);
                 
                 if(ArrayVisualizer.shuffleEnabled()) Delays.sleep(1);
             }
@@ -46,8 +62,10 @@ public enum Shuffles {
         public void shuffleArray(int[] array, ArrayVisualizer ArrayVisualizer, Delays Delays, Highlights Highlights, Writes Writes) {
             int currentLen = ArrayVisualizer.getCurrentLength();
             
-            for(int i = 0; i < currentLen; i++){
-                Writes.swap(array, i, (int)(Math.random()*i), 0, true, false);
+            for(int i = 1; i <= currentLen; i++){
+                int temp = (int)(Math.random()*i);
+                Reads.play2(array[i-1], array[temp]);
+                Writes.swap(array, i-1, temp, 0, true, false);
                 
                 if(ArrayVisualizer.shuffleEnabled()) Delays.sleep(1);
             }
@@ -59,7 +77,9 @@ public enum Shuffles {
             int currentLen = ArrayVisualizer.getCurrentLength();
             
             for(int i = 0; i < currentLen; i++){
-                Writes.swap(array, i, (int)(Math.random()*currentLen), 0, true, false);
+                int temp = (int)(Math.random()*currentLen);
+                Reads.play2(array[i], array[temp]);
+                Writes.swap(array, i, temp, 0, true, false);
                 
                 if(ArrayVisualizer.shuffleEnabled()) Delays.sleep(1);
             }
@@ -71,12 +91,16 @@ public enum Shuffles {
             int currentLen = ArrayVisualizer.getCurrentLength();
             
             for(int i = 0; i < Math.max(currentLen / 20, 1); i++){
-                Writes.swap(array, (int)(Math.random()*currentLen), (int)(Math.random()*currentLen), 0, true, false);
+                int temp = (int)(Math.random()*currentLen);
+                int temp2 = (int)(Math.random()*currentLen);
+                Reads.play2(array[temp], array[temp2]);
+                Writes.swap(array, temp, temp2, 0, true, false);
                 
                 if(ArrayVisualizer.shuffleEnabled()) Delays.sleep(2);
             }
             for (int left = 0, right = currentLen - 1; left < right; left++, right--) {
                 // swap the values at the left and right indices
+                Reads.play2(array[left], array[right]);
                 Writes.swap(array, left, right, 0, true, false);
                 
                 if(ArrayVisualizer.shuffleEnabled()) Delays.sleep(1);
@@ -89,12 +113,16 @@ public enum Shuffles {
             int currentLen = ArrayVisualizer.getCurrentLength();
             
             for(int i = 0; i < Math.max(Math.min((currentLen + 3) / 4, 64), 1); i++){
-                Writes.swap(array, (int)(Math.random()*currentLen), (int)(Math.random()*currentLen), 0, true, false);
+                int temp = (int)(Math.random()*currentLen);
+                int temp2 = (int)(Math.random()*currentLen);
+                Reads.play2(array[temp], array[temp2]);
+                Writes.swap(array, temp, temp2, 0, true, false);
                 
                 if(ArrayVisualizer.shuffleEnabled()) Delays.sleep(2);
             }
             for (int left = 0, right = currentLen - 1; left < right; left++, right--) {
                 // swap the values at the left and right indices
+                Reads.play2(array[left], array[right]);
                 Writes.swap(array, left, right, 0, true, false);
                 
                 if(ArrayVisualizer.shuffleEnabled()) Delays.sleep(1);
@@ -110,6 +138,7 @@ public enum Shuffles {
                 int i = n;
                 while(i<currentLen){
                     if ((int)(Math.random()*2)==0){
+                Reads.play2(array[i-n], array[i]);
                         Writes.swap(array, i - n, i, 0, true, false);
                         if(ArrayVisualizer.shuffleEnabled()) Delays.sleep(0.5);
                     }
@@ -123,6 +152,7 @@ public enum Shuffles {
                 i = (2*n);
                 while(i<currentLen){
                     if ((int)(Math.random()*2)==0){
+                Reads.play2(array[i-n], array[i]);
                         Writes.swap(array, i - n, i, 0, true, false);
                         if(ArrayVisualizer.shuffleEnabled()) Delays.sleep(0.5);
                     }
@@ -135,6 +165,7 @@ public enum Shuffles {
             }
             for (int left = 0, right = currentLen - 1; left < right; left++, right--) {
                 // swap the values at the left and right indices
+                Reads.play2(array[left], array[right]);
                 Writes.swap(array, left, right, 0, true, false);
                 
                 if(ArrayVisualizer.shuffleEnabled()) Delays.sleep(1);
@@ -148,6 +179,7 @@ public enum Shuffles {
             
             for (int left = 0, right = currentLen - 1; left < right; left++, right--) {
                 // swap the values at the left and right indices
+                Reads.play2(array[left], array[right]);
                 Writes.swap(array, left, right, 0, true, false);
                 
                 if(ArrayVisualizer.shuffleEnabled()) Delays.sleep(1);
@@ -161,16 +193,20 @@ public enum Shuffles {
             
             for(int i = 0; i < currentLen - 8; i++) {
                 array[i] = currentLen / 2;
+                Reads.play(array[i]);
                 
                 if(ArrayVisualizer.shuffleEnabled()) Delays.sleep(1);
             }
             for(int i = currentLen - 8; i < currentLen; i++) {
                 array[i] = (int) (Math.random() < 0.5 ? currentLen * 0.75 : currentLen * 0.25);
+                Reads.play(array[i]);
                 
                 if(ArrayVisualizer.shuffleEnabled()) Delays.sleep(1);
             }
             for(int i = 0; i < currentLen; i++){
-                Writes.swap(array, i, (int)(Math.random()*currentLen), 0, true, false);
+                int temp = (int)(Math.random()*currentLen);
+                Reads.play2(array[i], array[temp]);
+                Writes.swap(array, i, temp, 0, true, false);
                 
                 if(ArrayVisualizer.shuffleEnabled()) Delays.sleep(1);
             }
@@ -182,7 +218,10 @@ public enum Shuffles {
             int currentLen = ArrayVisualizer.getCurrentLength();
             
             for(int i = 0; i < Math.max(currentLen / 20, 1); i++){
-                Writes.swap(array, (int)(Math.random()*currentLen), (int)(Math.random()*currentLen), 0, true, false);
+                int temp = (int)(Math.random()*currentLen);
+                int temp2 = (int)(Math.random()*currentLen);
+                Reads.play2(array[temp], array[temp2]);
+                Writes.swap(array, temp, temp2, 0, true, false);
                 
                 if(ArrayVisualizer.shuffleEnabled()) Delays.sleep(2);
             }
@@ -194,7 +233,10 @@ public enum Shuffles {
             int currentLen = ArrayVisualizer.getCurrentLength();
             
             for(int i = 0; i < Math.max(Math.min((currentLen + 3) / 4, 64), 1); i++){
-                Writes.swap(array, (int)(Math.random()*currentLen), (int)(Math.random()*currentLen), 0, true, false);
+                int temp = (int)(Math.random()*currentLen);
+                int temp2 = (int)(Math.random()*currentLen);
+                Reads.play2(array[temp], array[temp2]);
+                Writes.swap(array, temp, temp2, 0, true, false);
                 
                 if(ArrayVisualizer.shuffleEnabled()) Delays.sleep(2);
             }
@@ -209,6 +251,7 @@ public enum Shuffles {
                 int i = n;
                 while(i<currentLen){
                     if ((int)(Math.random()*2)==0){
+                Reads.play2(array[i-n], array[i]);
                         Writes.swap(array, i - n, i, 0, true, false);
                         if(ArrayVisualizer.shuffleEnabled()) Delays.sleep(0.5);
                     }
@@ -222,6 +265,7 @@ public enum Shuffles {
                 i = (2*n);
                 while(i<currentLen){
                     if ((int)(Math.random()*2)==0){
+                Reads.play2(array[i-n], array[i]);
                         Writes.swap(array, i - n, i, 0, true, false);
                         if(ArrayVisualizer.shuffleEnabled()) Delays.sleep(0.5);
                     }
@@ -241,6 +285,7 @@ public enum Shuffles {
             
             for(int i = 0; i < currentLen; i++) {
                 if(ArrayVisualizer.shuffleEnabled()) {
+                Reads.play(array[i]);
                     Highlights.markArray(1, i);
                     Delays.sleep(1);
                 }
@@ -255,7 +300,9 @@ public enum Shuffles {
             int few = 2;
             for(int i = 0; i < currentLen; i++) {
 		array[i] = ((((((i*few)/currentLen)*2)+1)*currentLen)/(few*2));
-                Writes.swap(array, i, (int)(Math.random()*i), 0, true, false);
+                int temp = (int)(Math.random()*(i+1));
+                Reads.play2(array[i], array[temp]);
+                Writes.swap(array, i, temp, 0, true, false);
                 if(ArrayVisualizer.shuffleEnabled()) {
                     Highlights.markArray(1, i);
                     Delays.sleep(1);
@@ -271,7 +318,9 @@ public enum Shuffles {
             int few = 4;
             for(int i = 0; i < currentLen; i++) {
 		array[i] = ((((((i*few)/currentLen)*2)+1)*currentLen)/(few*2));
-                Writes.swap(array, i, (int)(Math.random()*i), 0, true, false);
+                int temp = (int)(Math.random()*(i+1));
+                Reads.play2(array[i], array[temp]);
+                Writes.swap(array, i, temp, 0, true, false);
                 if(ArrayVisualizer.shuffleEnabled()) {
                     Highlights.markArray(1, i);
                     Delays.sleep(1);
@@ -287,7 +336,9 @@ public enum Shuffles {
             int few = 8;
             for(int i = 0; i < currentLen; i++) {
 		array[i] = ((((((i*few)/currentLen)*2)+1)*currentLen)/(few*2));
-                Writes.swap(array, i, (int)(Math.random()*i), 0, true, false);
+                int temp = (int)(Math.random()*(i+1));
+                Reads.play2(array[i], array[temp]);
+                Writes.swap(array, i, temp, 0, true, false);
                 if(ArrayVisualizer.shuffleEnabled()) {
                     Highlights.markArray(1, i);
                     Delays.sleep(1);
@@ -303,7 +354,9 @@ public enum Shuffles {
             int few = 16;
             for(int i = 0; i < currentLen; i++) {
 		array[i] = ((((((i*few)/currentLen)*2)+1)*currentLen)/(few*2));
-                Writes.swap(array, i, (int)(Math.random()*i), 0, true, false);
+                int temp = (int)(Math.random()*(i+1));
+                Reads.play2(array[i], array[temp]);
+                Writes.swap(array, i, temp, 0, true, false);
                 if(ArrayVisualizer.shuffleEnabled()) {
                     Highlights.markArray(1, i);
                     Delays.sleep(1);
@@ -318,6 +371,7 @@ public enum Shuffles {
             
             for (int left = 0, right = currentLen - 1; left < right; left++, right--) {
                 // swap the values at the left and right indices
+                Reads.play2(array[left], array[right]);
                 Writes.swap(array, left, right, 0, true, false);
                 
                 if(ArrayVisualizer.shuffleEnabled()) Delays.sleep(1);
@@ -333,6 +387,7 @@ public enum Shuffles {
             int currentLen = ArrayVisualizer.getCurrentLength();
             
             for(int i = 0; i < currentLen/2; i++) {
+                Reads.play2(array[i], array[i+(currentLen/2)]);
                 Writes.swap(array, i, i+(currentLen/2), 0, true, false);
                 
                 if(ArrayVisualizer.shuffleEnabled()) Delays.sleep(1);
@@ -351,7 +406,8 @@ public enum Shuffles {
                     j += k % 2;
                     k /= 2;
                 }
-                if (j > i){
+                if (j > i && j < currentLen){
+                Reads.play2(array[i], array[j]);
                     Writes.swap(array, i, j, 0, true, false);
                     if(ArrayVisualizer.shuffleEnabled()) Delays.sleep(1);
                 }
@@ -364,7 +420,9 @@ public enum Shuffles {
             int currentLen = ArrayVisualizer.getCurrentLength();
             for(int i = 0; i < currentLen; i++) {
 		array[i] = 3*(int)(((Math.pow((((i+0.0)/currentLen)*2.0)-1.0, 3)+1.0)/6.0)*currentLen);
-                Writes.swap(array, i, (int)(Math.random()*i), 0, true, false);
+                int temp = (int)(Math.random()*(i+1));
+                Reads.play2(array[i], array[temp]);
+                Writes.swap(array, i, temp, 0, true, false);
                 if(ArrayVisualizer.shuffleEnabled()) {
                     Highlights.markArray(1, i);
                     Delays.sleep(1);
@@ -378,7 +436,9 @@ public enum Shuffles {
             int currentLen = ArrayVisualizer.getCurrentLength();
             for(int i = 0; i < currentLen; i++) {
 		array[i] = 3*(int)(((Math.pow((((i+0.0)/currentLen)*2.0)-1.0, 5)+1.0)/6.0)*currentLen);
-                Writes.swap(array, i, (int)(Math.random()*i), 0, true, false);
+                int temp = (int)(Math.random()*(i+1));
+                Reads.play2(array[i], array[temp]);
+                Writes.swap(array, i, temp, 0, true, false);
                 if(ArrayVisualizer.shuffleEnabled()) {
                     Highlights.markArray(1, i);
                     Delays.sleep(1);
@@ -393,6 +453,7 @@ public enum Shuffles {
             for(int i = 0; i < currentLen; i++) {
 		array[i] = 3*(int)(((Math.pow((((i+0.0)/currentLen)*2.0)-1.0, 3)+1.0)/6.0)*currentLen);
                 if(ArrayVisualizer.shuffleEnabled()) {
+                Reads.play(array[i]);
                     Highlights.markArray(1, i);
                     Delays.sleep(1);
                 }
@@ -406,6 +467,7 @@ public enum Shuffles {
             for(int i = 0; i < currentLen; i++) {
 		array[i] = 3*(int)(((Math.pow((((i+0.0)/currentLen)*2.0)-1.0, 5)+1.0)/6.0)*currentLen);
                 if(ArrayVisualizer.shuffleEnabled()) {
+                Reads.play(array[i]);
                     Highlights.markArray(1, i);
                     Delays.sleep(1);
                 }
@@ -419,6 +481,7 @@ public enum Shuffles {
             for(int i = 0; i < currentLen; i++) {
 		array[i] = 3*(int)(((Math.pow((((currentLen-i-1.0)/currentLen)*2.0)-1.0, 3)+1.0)/6.0)*currentLen);
                 if(ArrayVisualizer.shuffleEnabled()) {
+                Reads.play(array[i]);
                     Highlights.markArray(1, i);
                     Delays.sleep(1);
                 }
@@ -432,6 +495,7 @@ public enum Shuffles {
             for(int i = 0; i < currentLen; i++) {
 		array[i] = 3*(int)(((Math.pow((((currentLen-i-1.0)/currentLen)*2.0)-1.0, 5)+1.0)/6.0)*currentLen);
                 if(ArrayVisualizer.shuffleEnabled()) {
+                Reads.play(array[i]);
                     Highlights.markArray(1, i);
                     Delays.sleep(1);
                 }
